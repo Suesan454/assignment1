fc9fc27673b27e663faf847e785be9baa8339246
Albert Izzo <jubalsfriend@gmail.com>
2021-05-12 14:19:02 -0400

    done now

--- idone/src/main/java/idone/driver.java
+++ idone/src/main/java/idone/driver.java
@@ -7,8 +7,12 @@ public class driver {
 		
 		
 		cardriver Lewis = new cardriver("Hamilton", 68, 30);
+		cardriver Fernando = new cardriver("Alonzo", 68, 30);
 		
+		cartech ToTo = new cartech("Wolf", 68, 30);
+		cartech John = new cartech("Doe", 68, 30);
 		
+		System.out.println("Our trainer is cool.");
 		
 	}
 	
